87fc62b04452a350d46fdfb4e27c45703fdb27f9
Charles Ewel <Charles.Ewel@gmail.com>
2016-08-12 09:01:19 -0700

    added form to create new meal

--- app/create-new-meal.component.ts
+++ app/create-new-meal.component.ts
@@ -1,16 +1,36 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
 import { Meal } from './meal.model';
 
 
 @Component({
   selector: 'create-new-meal',
+  outputs: ['onAddNewMeal'],
   template: `
-
+  <h3>Add Meal:</h3>
+  <input placeholder="Name" class="col-sm-8 input-lg" #newName>
+  <input placeholder="Details" class="col-sm-8 input-lg" #newDetails>
+  <input type="number" placeholder="Calories" class="col-sm-8 input-lg" #newCalories>
+  <button (click)="addReview(newName, newDetails, newCalories)" class="add-button btn-lg">Add</button>
   `
 })
 
 export class CreateNewMealComponent {
-
+  public onAddNewMeal: EventEmitter<string[]>;
   constructor() {
+    this.onAddNewMeal = new EventEmitter();
+  }
+  addReview(userName: HTMLInputElement, userDetails: HTMLInputElement, userCalories: HTMLInputElement) {
+    if(userName.value === "" || userDetails.value === "" || userCalories.value === "") {
+      alert("You didn't fill out all the form fields")
+    } else {
+      var emission = [];
+      emission.push(userName.value);
+      emission.push(userDetails.value);
+      emission.push(userCalories.value);
+      this.onAddNewMeal.emit(emission);
+      userName.value = "";
+      userDetails.value = "";
+      userCalories.value = "";
+    }
   }
 }
--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -1,21 +1,30 @@
 import { Component } from 'angular2/core';
 import { Meal } from './meal.model';
 import { MealComponent } from './meal.component';
+import { CreateNewMealComponent } from './create-new-meal.component';
 
 
 @Component({
   selector: 'meal-list',
   inputs: ['meals'],
-  directives: [MealComponent],
+  directives: [MealComponent, CreateNewMealComponent],
   template: `
   <meal-display *ngFor="#currentMeal of meals"
     [meal]="currentMeal">
   </meal-display>
+  <create-new-meal
+    (onAddNewMeal)="createMeal($event[0], $event[1], $event[2])">
+  </create-new-meal>
   `
 })
 
 export class MealListComponent {
   public meals: Meal[];
   constructor() {
+  }
+  createMeal(name: string, details: string, calories: string) {
+    this.meals.push(
+      new Meal(name, details, parseInt(calories), this.meals.length)
+    )
   }
 }
